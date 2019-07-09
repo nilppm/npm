acadc48242e5319893754b05cab954688a4a5059
evio <evio@vip.qq.com>
2019-07-09 17:51:42 +0800

    update readme <add update sql>

--- cpm-to-nilppm.sql
+++ cpm-to-nilppm.sql
@@ -2,4 +2,6 @@ alter table maintainer ADD utime datetime NOT NULL;
 CREATE INDEX idx_account ON user (account);
 alter table version ADD mtime datetime NOT NULL;
 CREATE INDEX idx_rev ON version (rev);
-CREATE UNIQUE INDEX uq_pid_name ON version (`pid`, `name`);
+CREATE UNIQUE INDEX uq_pid_name ON `version` (`pid`, `name`);
+update maintainer set utime=ctime;
+update `version` set mtime=ctime;
